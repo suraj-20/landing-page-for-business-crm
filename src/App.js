cb2e38335221136d82d60967b93bb9b8d201592a
Suraj <kumarsuraj54057@gmail.com>
2023-09-03 13:50:38 +0530

    Main Section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Home from "./Components/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
+import Main from "./Components/Main/Main";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Main />
     </>
   );
 }
---
+++ src/Components/Main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+      Hare Krishna
+    </div>
+  )
+}
+
+export default Main
